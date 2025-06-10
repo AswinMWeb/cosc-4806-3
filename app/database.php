3a0a6c952ee7347b77e18746629aac9a43ab1eca
AswinMWeb <133523889+AswinMWeb@users.noreply.github.com>
2025-06-10 01:27:24 +0000

    Updated database .php by making it connect to database and error message to if database is down

--- app/database.php
+++ app/database.php
@@ -1,14 +1,13 @@
 <?php
 
-/* database connection stuff here
- * 
- */
-
 function db_connect() {
-    try { 
-        $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
+    try {
+        $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE;
+        $dbh = new PDO($dsn, DB_USER, DB_PASS);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbh;
     } catch (PDOException $e) {
-        //We should set a global variable here so we know the DB is down
+        error_log('Database Connection Error: ' . $e->getMessage());
+        return null;
     }
 }
